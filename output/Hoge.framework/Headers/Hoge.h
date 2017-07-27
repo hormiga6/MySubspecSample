1578ab84fec44f05b177717f0fa4b51ad6bc9349
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-07-27 09:56:39 +0900

    update

---
+++ output/Hoge.framework/Headers/Hoge.h
@@ -0,0 +1,21 @@
+//
+//  Hoge.h
+//  Hoge
+//
+//  Copyright © 2017年 hormiga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hoge.
+FOUNDATION_EXPORT double HogeVersionNumber;
+
+//! Project version string for Hoge.
+FOUNDATION_EXPORT const unsigned char HogeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hoge/PublicHeader.h>
+#import "HogeString.h"
+
+
+
+
---
+++ output/SubspecPod.framework/Headers/SubspecPodString.h
@@ -0,0 +1,13 @@
+//
+//  SubspecPodString.h
+//  SubspecPod
+//
+//  Created by 有村 皓太郎 on 2017/07/26.
+//  Copyright © 2017年 hormiga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubspecPodString : NSObject
++(NSString*)getHogeString;
+@end
